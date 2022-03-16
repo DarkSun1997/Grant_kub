9ff674fd9ee460bdc39339e75e882a21f1a167fa
aleksandrpahomov <sanek10121998@gmail.com>
2022-03-16 12:42:17 +0300

    edit norm func and add U, B1, B2 functions

--- main.py
+++ main.py
@@ -2,41 +2,69 @@ import json
 import math
 from scipy import integrate
 
+
 def fi1(a, m, T, tau):
     return math.exp((-a) / m * (T - tau)) / m
+
 
 def fi2(a, m, T, tau):
     return (1 - math.exp((-a) / m * (T - tau))) / a
 
+
 def g1(dparametr_T, dparametr_0, a, m, T):
     return dparametr_T - dparametr_0 * math.exp((-a) / m * T)
 
+
 def g2(parametr_T, parametr_0, dparametr_0, a, m, T):
     return parametr_T - parametr_0 - dparametr_0 * (1 - math.exp((-a) / m * T)) * m / a
+
 
 def z(a, m, T, tau):
     print(tau)
     return tau
 
-def norm(func, a, m, T, tau):
+
+def norm(func, a, m, T):
     func_integrate = lambda tau_integrate: func(a, m, T, tau_integrate)
-    return integrate.quad(func_integrate**2, 0, T)[0]
+    return integrate.quad(func_integrate ** 2, 0, T)[0]
+
 
 def psi1(a, m, T, tau):
-    fi1_value=fi1(a, m, T, tau)
-    fi1_norm = norm(fi1, a, m, T, tau)
+    fi1_value = fi1(a, m, T, tau)
+    fi1_norm = norm(fi1, a, m, T)
     return fi1_value / fi1_norm
+
 
 def psi2(a, m, T, tau):
     psi2_hut_value = psi2_hut(a, m, T, tau)
-    psi2_hut_norm = norm(psi2_hut,a , m, T, tau)
-    return  psi2_hut_value/psi2_hut_norm
+    psi2_hut_norm = norm(psi2_hut, a, m, T)
+    return psi2_hut_value / psi2_hut_norm
+
 
 def psi2_hut(a, m, T, tau):
     fi2_integrate = lambda tau_integrate: fi2(a, m, T, tau_integrate)
     psi1_integrate = lambda tau_integrate: psi1(a, m, T, tau_integrate)
-    a_21=- integrate.quad(fi2_integrate*psi1_integrate, 0, T)[0]
-    return a_21*psi1(a, m, T, tau) + fi2(a, m, T, tau)
+    a_21 = - integrate.quad(fi2_integrate * psi1_integrate, 0, T)[0]
+    return a_21 * psi1(a, m, T, tau) + fi2(a, m, T, tau)
+
+
+def B1(dparametr_T, dparametr_0, a, m, T):
+    b_11 = 1 / norm(fi1, a, m, T)
+    return b_11 * g1(dparametr_T, dparametr_0, a, m, T)
+
+
+def B2(parametr_T, parametr_0, dparametr_T, dparametr_0, a, m, T):
+    fi2_integrate = lambda tau_integrate: fi2(a, m, T, tau_integrate)
+    psi1_integrate = lambda tau_integrate: psi1(a, m, T, tau_integrate)
+    a_21 = - integrate.quad(fi2_integrate * psi1_integrate, 0, T)[0]
+    b_21 = a_21 / (norm(psi2_hut, a, m, T) * norm(fi1, a, m, T))
+    b_22 = 1 / norm(psi2_hut, a, m, T)
+    return b_21 * g1(dparametr_T, dparametr_0, a, m, T) + b_22 * g2(parametr_T, parametr_0, dparametr_0, a, m, T)
+
+
+def U(parametr_T, parametr_0, dparametr_T, dparametr_0, a, m, T, tau):
+    return B1(dparametr_T, dparametr_0, a, m, T) * psi1(a, m, T, tau) + B2(parametr_T, parametr_0, dparametr_T,
+                                                                           dparametr_0, a, m, T) * psi2(a, m, T, tau)
 
 
 def normal_func_L2(func, a, m, T, time_start, time_finish):
@@ -44,7 +72,7 @@ def normal_func_L2(func, a, m, T, time_start, time_finish):
     step = (time_finish - time_start) / 100
     time = time_start
     while time_finish - time > 0.00001:
-        sum = sum + (func(a, m, T, tau = time + step / 2) )  * step
+        sum = sum + (func(a, m, T, tau=time + step / 2)) * step
         print(sum)
         time = time + step
     return sum
