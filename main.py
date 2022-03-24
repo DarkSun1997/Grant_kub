51308519006c653be9fe1a68ef4b5d1199210bfe
maslenikov <igor.maslenikov16@yandex.ru>
2022-03-24 16:33:56 +0300

    1 siple ris

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import info_cache
 import kub_func
 from scipy import integrate
 import matplotlib.pyplot as plt
+import numpy as np
 
 def eler(q,
          w,
@@ -30,7 +31,6 @@ def transmission_of_information():
 
 def generate_information_calc_BLA():
     for i in range(len(info_cache.info_BLA)):
-        #print(info_cache.info_BLA[i])
         info_cache.info_BLA[i]["x"]["a21"] = None
         info_cache.info_BLA[i]["y"]["a21"] = None
         info_cache.info_BLA[i]["z"]["a21"] = None
@@ -58,16 +58,11 @@ def generate_information_calc_BLA():
         info_cache.info_BLA[i]["x"]["b21"] = None
         info_cache.info_BLA[i]["y"]["b21"] = None
         info_cache.info_BLA[i]["z"]["b21"] = None
-        #print(info_cache.info_BLA[i])
 
 
 
 transmission_of_information()
 print(info_cache.info_BLA)
-
-
-
-
 
 
 result_x = []
@@ -83,6 +78,80 @@ while abs(info_cache.time_finish - time) > 0.00000001:
     time = time + time_step
 
 print(result_x)
+
+
+result_y = []
+time_step = 0.001
+time = info_cache.time_start
+gg = 0
+e = [info_cache.info_BLA[0]["y"]["0"],info_cache.info_BLA[0]["y"]["d0"]]
+result_y.append(e)
+print(e)
+while abs(info_cache.time_finish - time) > 0.00000001:
+    e = eler(result_y[len(result_y)-1][0], result_y[len(result_y)-1][1], time, time_step, 0, "y", gg)
+    result_y.append(e)
+    time = time + time_step
+
+print(result_y)
+
+full_res = []
+for i in range(len(result_x)):
+    full_res.append(result_x[i][0])
+
+full_res1_x = full_res
+full_res = []
+for i in range(len(result_y)):
+    full_res.append(result_y[i][0])
+
+full_res1_y = full_res
+plt.plot(full_res1_x,full_res1_y)
+
+
+
+
+result_x = []
+time_step = 0.001
+time = info_cache.time_start
+gg = 0
+e = [info_cache.info_BLA[1]["x"]["0"],info_cache.info_BLA[0]["x"]["d0"]]
+result_x.append(e)
+print(e)
+while abs(info_cache.time_finish - time) > 0.00000001:
+    e = eler(result_x[len(result_x)-1][0], result_x[len(result_x)-1][1], time, time_step, 1, "x", gg)
+    result_x.append(e)
+    time = time + time_step
+
+print(result_x)
+
+
+result_y = []
+time_step = 0.001
+time = info_cache.time_start
+gg = 0
+e = [info_cache.info_BLA[1]["y"]["0"],info_cache.info_BLA[0]["y"]["d0"]]
+result_y.append(e)
+print(e)
+while abs(info_cache.time_finish - time) > 0.00000001:
+    e = eler(result_y[len(result_y)-1][0], result_y[len(result_y)-1][1], time, time_step, 1, "y", gg)
+    result_y.append(e)
+    time = time + time_step
+
+print(result_y)
+
+full_res = []
+for i in range(len(result_x)):
+    full_res.append(result_x[i][0])
+
+full_res1_x = full_res
+full_res = []
+for i in range(len(result_y)):
+    full_res.append(result_y[i][0])
+
+full_res1_y = full_res
+plt.plot(full_res1_x,full_res1_y)
+
+
+plt.show()
 """
 generate_information_calc_BLA(Data)
 
