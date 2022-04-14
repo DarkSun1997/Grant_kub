b9c6e5c4ab4fac3f4b6845ddffec8413479a0bab
maslenikov <igor.maslenikov16@yandex.ru>
2022-04-14 16:12:04 +0300

    123 nedodelka

--- Runge_Kutta.py
+++ Runge_Kutta.py
@@ -11,3 +11,18 @@ def eler(q,
     dq = q + time_step * w
     dw = w + time_step * ((kub_func.U(id, axis, time) - info_cache.info_BLA[id]["a"] * w) / info_cache.info_BLA[id]["m"])
     return dq, dw
+
+
+def func(pred_val,
+         pred_dval,
+         time,
+         time_step,
+         id,
+         axis,
+         tau,
+         number):
+    
+    val = pred_val + time_step * pred_dval
+    dval = pred_dval + time_step * (
+                (kub_func.U(id, axis, time) - info_cache.info_BLA[id]["a"] * pred_dval) / info_cache.info_BLA[id]["m"])
+    return val, dval
