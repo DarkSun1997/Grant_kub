72445cc6625ed1ed4ffc82a053858c8030107410
maslenikov <igor.maslenikov16@yandex.ru>
2022-04-14 13:53:27 +0300

    update

--- main.py
+++ main.py
@@ -65,43 +65,35 @@ transmission_of_information()
 print(info_cache.info_BLA)
 
 result = []
+for id in range(len(info_cache.info_BLA)):
+    result.append({"x": [], "y": [], "z": []})
+    e = [info_cache.info_BLA[id]["x"]["0"], info_cache.info_BLA[id]["x"]["d0"]]
+    result[id]["x"].append(e)
+    e = [info_cache.info_BLA[id]["y"]["0"], info_cache.info_BLA[id]["y"]["d0"]]
+    result[id]["y"].append(e)
+    e = [info_cache.info_BLA[id]["z"]["0"], info_cache.info_BLA[id]["z"]["d0"]]
+    result[id]["z"].append(e)
+
+time_step = 0.001
+time = info_cache.time_start
+gg = 0
+while abs(info_cache.time_finish - time) > 0.00000001:
+    for id in range(len(info_cache.info_BLA)):
+        e = Runge_Kutta.eler(result[id]["x"][len(result[id]["x"]) - 1][0], result[id]["x"][len(result[id]["x"]) - 1][1],
+                                 time, time_step, id, "x", gg)
+        result[id]["x"].append(e)
+        e = Runge_Kutta.eler(result[id]["y"][len(result[id]["y"]) - 1][0], result[id]["y"][len(result[id]["y"]) - 1][1],
+                                 time, time_step, id, "y", gg)
+        result[id]["y"].append(e)
+
+        e = Runge_Kutta.eler(result[id]["z"][len(result[id]["z"]) - 1][0], result[id]["z"][len(result[id]["z"]) - 1][1],
+                                 time, time_step, id, "y", gg)
+        result[id]["z"].append(e)
+    time = time + time_step
+
+
 
 for id in range(len(info_cache.info_BLA)):
-    time_step = 0.001
-    time = info_cache.time_start
-    gg = 0
-    e = [info_cache.info_BLA[id]["x"]["0"], info_cache.info_BLA[id]["x"]["d0"]]
-    result.append({"x": [], "y": [], "z": []})
-    result[id]["x"].append(e)
-    while abs(info_cache.time_finish - time) > 0.00000001:
-        e = Runge_Kutta.eler(result[id]["x"][len(result[id]["x"]) - 1][0], result[id]["x"][len(result[id]["x"]) - 1][1],
-                             time, time_step, id, "x", gg)
-        result[id]["x"].append(e)
-        time = time + time_step
-
-    time_step = 0.001
-    time = info_cache.time_start
-    gg = 0
-    e = [info_cache.info_BLA[id]["y"]["0"], info_cache.info_BLA[id]["y"]["d0"]]
-    result[id]["y"] = []
-    result[id]["y"].append(e)
-    while abs(info_cache.time_finish - time) > 0.00000001:
-        e = Runge_Kutta.eler(result[id]["y"][len(result[id]["y"]) - 1][0], result[id]["y"][len(result[id]["y"]) - 1][1],
-                             time, time_step, id, "y", gg)
-        result[id]["y"].append(e)
-        time = time + time_step
-
-    time_step = 0.001
-    time = info_cache.time_start
-    gg = 0
-    e = [info_cache.info_BLA[id]["z"]["0"], info_cache.info_BLA[id]["z"]["d0"]]
-    result[id]["z"] = []
-    result[id]["z"].append(e)
-    while abs(info_cache.time_finish - time) > 0.00000001:
-        e = Runge_Kutta.eler(result[id]["z"][len(result[id]["z"]) - 1][0], result[id]["z"][len(result[id]["z"]) - 1][1],
-                             time, time_step, id, "y", gg)
-        result[id]["z"].append(e)
-        time = time + time_step
 
     full_res = []
     for i in range(len(result[id]["x"])):
@@ -114,7 +106,20 @@ for id in range(len(info_cache.info_BLA)):
 
     full_res1_y = full_res
     plt.plot(full_res1_x, full_res1_y)
-    """
+
+"""
+    full_res = []
+    for i in range(len(result[id]["x"])):
+        full_res.append(result[id]["x"][i][0])
+
+    full_res1_x = full_res
+    full_res = []
+    for i in range(len(result[id]["y"])):
+        full_res.append(result[id]["y"][i][0])
+
+    full_res1_y = full_res
+    plt.plot(full_res1_x, full_res1_y)
+    
     full_res = []
     for i in range(len(result[id]["z"])):
         full_res.append(result[id]["z"][i][0])
